3e3da5d074744cc970a5764b3a067754667ead61
soop <77305497+th3n0y0u@users.noreply.github.com>
2021-06-24 13:56:37 -0700

    Update main.lua

--- main.lua
+++ main.lua
@@ -4,139 +4,123 @@ local debounce = false
 
 local canGive = false
 
-local tool = game.ReplicatedStorage:FindFirstChild("HyperlaserGun")
-
 local player = script.Parent.Parent.Parent.Parent
 
-function onEntered(enterPressed)
-	if debounce == false then
-		if game.Players:FindFirstChild(script.Parent.FreezePlayer.Text) then
-			local target = game.Players:FindFirstChild(script.Parent.FreezePlayer.Text)
-			if target.Character ~= nil then
-				if target ~= script.Parent.Parent.Name then
-					target.Character.HumanoidRootPart.Anchored = true
+local tool = game.ReplicatedStorage:FindFirstChild("HyperlaserGun")
+
+function mainclass()
+
+	local function sound()
+		local freeze = Instance.new("Sound")
+		freeze.Name = "FreezeSound"
+		freeze.SoundId = "rbxassetid://2364677393" 
+		freeze.PlaybackSpeed = 1
+		freeze.Volume = 1
+		freeze:Play()
+		coroutine.resume(coroutine.create(function()
+			wait(freeze.TimeLength) 
+			freeze:Destroy()
+		end) )
+	end
+	
+	local function onentered(player, text)
+		if debounce == false then
+			if game.Players:FindFirstChild(text) then
+				local target = game.Players:FindFirstChild(text)
+				if target.Character ~= nil then
+					if tostring(target) ~= script.Parent.Parent.Parent.Parent.Name then
+						target.Character.HumanoidRootPart.Anchored = true
+					elseif tostring(target) == script.Parent.Parent.Parent.Parent.Name then
+						print("stop freezing yourself nerd")
+					end
 				else
-					print("stop freezing yourself nerd")
+					print("stop it")
 				end
-			else
-				print("stop it")
 			end
 		end
 	end
-end
-
-function sound()
-	local freeze = Instance.new("Sound")
-	freeze.Name = "FreezeSound"
-	freeze.SoundId = "rbxassetid://2364677393" 
-	freeze.PlaybackSpeed = 1
-	freeze.Volume = 1
-	freeze:Play()
-	coroutine.resume(coroutine.create(function()
-		wait(freeze.TimeLength) 
-		freeze:Destroy()
-	end) )
-end
-
-local function fired2()
-	script.Parent.FreezeAllPlayers.Text = "INITALIZING..."
-	wait(3)
-	for i, player in pairs(Players:GetPlayers()) do
-		if player.Name ~= script.Parent.Parent.Name then
-			player.Character.HumanoidRootPart.Anchored = true
-		else
-			print("No freezing yourself.")
-		end
-	end
-	wait(3)
-	script.Parent.FreezeAllPlayers.Text = "Fired.."
-	sound()
-	wait(1.5)
-	script.Parent.FreezeAllPlayers.Text = "FREEZE ALL PLAYERS"
-end
-
-local function fired()
-	script.Parent.FreezeAllPlayers.Text = "Are you sure that you want to do this?"
-	script.Parent.FreezeAllPlayers.MouseButton1Down:Connect(function(click)
-		fired2() 
-	end)
-end
-
-function notonEntered(enterPressed)
-	if debounce == false then
-		if game.Players:FindFirstChild(script.Parent.UnfreezePlayer.Text) then
-			local target = game.Players:FindFirstChild(script.Parent.UnfreezePlayer.Text)
-			if target.Character ~= nil then
-				if target ~= script.Parent.Parent.Name then
-					target.Character.HumanoidRootPart.Anchored = false
+	
+	local function notonentered(player, text)
+		if debounce == false then
+			if game.Players:FindFirstChild(text) then
+				local target = game.Players:FindFirstChild(text)
+				if target.Character ~= nil then
+					if tostring(target) ~= script.Parent.Parent.Parent.Parent.Name then
+						target.Character.HumanoidRootPart.Anchored = false
+					elseif tostring(target) == script.Parent.Parent.Parent.Parent.Name then
+						print("stop unfreezing yourself nerd")
+					end
 				else
-					print("stop unfreezing yourself nerd")
+					print("stop it")
 				end
-			else
-				print("stop it")
 			end
 		end
 	end
-end
-local function unfired2()
-	script.Parent.UnFreezeAllPlayers.Text = "INITALIZING..."
-	wait(3)
-	for i, player in pairs(Players:GetPlayers()) do
-		if player.Name ~= script.Parent.Parent.Name then
-			player.Character.HumanoidRootPart.Anchored = false
-		else
-			print("No unfreezing yourself.")
+	
+	local function fired2()
+		script.Parent.FreezeAllPlayers.Text = "INITALIZING..."
+		wait(3)
+		for i, player in pairs(Players:GetPlayers()) do
+			if player.Name ~= script.Parent.Parent.Parent.Parent.Name then
+				player.Character.HumanoidRootPart.Anchored = true
+			elseif player.Name == script.Parent.Parent.Parent.Parent.Name then
+				print("No freezing yourself.")
+			end
+		end
+		wait(3)
+		script.Parent.FreezeAllPlayers.Text = "Fired.."
+		sound()
+		wait(1.5)
+		script.Parent.FreezeAllPlayers.Text = "FREEZE ALL PLAYERS"
+	end
+
+	local function fired()
+		script.Parent.FreezeAllPlayers.Text = "Are you sure that you want to do this?"
+		script.Parent.FreezeAllPlayers.MouseButton1Down:Connect(function(click)
+			fired2() 
+		end)
+	end
+
+	local function unfired2()
+		script.Parent.UnFreezeAllPlayers.Text = "INITALIZING..."
+		wait(3)
+		for i, player in pairs(Players:GetPlayers()) do
+			if player.Name ~= script.Parent.Parent.Parent.Parent.Name then
+				player.Character.HumanoidRootPart.Anchored = false
+			elseif player.Name == script.Parent.Parent.Parent.Parent.Name then
+				print("No unfreezing yourself.")
+			end
+		end
+		wait(3)
+		script.Parent.UnFreezeAllPlayers.Text = "Fired.."
+		wait(1.5)
+		script.Parent.UnFreezeAllPlayers.Text = "UNFREEZE ALL PLAYERS"
+	end
+
+	local function unfired()
+		script.Parent.UnFreezeAllPlayers.Text = "Are you sure that you want to do this?"
+		script.Parent.UnFreezeAllPlayers.MouseButton1Down:Connect(function(click)
+			unfired2() 
+		end)
+	end 
+	
+	local function item(player, text)
+		if canGive == false then
+			canGive = true
+			for v = 1, tonumber(text), 1 do
+				local clone = tool:Clone()
+				clone.Parent = player.Backpack 
+			end
+			wait(1)
+			canGive = false
 		end
 	end
-	wait(3)
-	script.Parent.UnFreezeAllPlayers.Text = "Fired.."
-	wait(1.5)
-	script.Parent.UnFreezeAllPlayers.Text = "UNFREEZE ALL PLAYERS"
+
+	script.Parent.FreezeAllPlayers.MouseButton1Down:Connect(fired)
+	script.Parent.UnFreezeAllPlayers.MouseButton1Down:Connect(unfired)
+	script.Parent.CanGive.OnServerEvent:Connect(item)
+	script.Parent.OnEntered.OnServerEvent:Connect(onentered)
+	script.Parent.NotOnEntered.OnServerEvent:Connect(notonentered)
 end
 
-local function unfired()
-	script.Parent.UnFreezeAllPlayers.Text = "Are you sure that you want to do this?"
-	script.Parent.UnFreezeAllPlayers.MouseButton1Down:Connect(function(click)
-		unfired2() 
-	end)
-end 
-
-
-local function item()
-	if canGive == false then
-		canGive = true
-		for v = 1, tonumber(script.Parent.UserInput.Text), 1 do
-			local clone = tool:Clone()
-			clone.Parent = player.Backpack 
-		end
-		wait(1)
-		canGive = false
-	end
-end
-
-local function prompt()
-	script.Parent.UserInput.Text = "Please input a number here for many laser guns"
-	script.Parent.UserInput.FocusLost:Connect(function(player)
-		item()
-	end)
-end 
-
-script.Parent.Gun.MouseButton1Down:Connect(function(player)
-	prompt()
-end) 
-
-script.Parent.FreezeAllPlayers.MouseButton1Down:Connect(fired)
-script.Parent.UnFreezeAllPlayers.MouseButton1Down:Connect(unfired)
-script.Parent.FreezePlayer.FocusLost:Connect(onEntered) 
-script.Parent.UnfreezePlayer.FocusLost:Connect(notonEntered)  
-
--- Exit and Enter 
-function exitandenter()
-	if script.Parent.Parent.Parent.Frame.Visible == false then
-		script.Parent.Parent.Parent.Frame.Visible = true
-	else
-		script.Parent.Parent.Parent.Frame.Visible = false 
-	end
-end
-
-script.Parent.MouseButton1Down:Connect(exitandenter) 
+mainclass()
